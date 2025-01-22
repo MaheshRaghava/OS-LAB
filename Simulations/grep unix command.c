12c37117c9a7dee2f89cbabbd139702db4db9a35
Mahesh Raghava <160461240+MaheshRaghava@users.noreply.github.com>
2025-01-22 12:18:10 +0530

    Update grep unix command.c

--- Simulations/grep unix command.c
+++ Simulations/grep unix command.c
@@ -2,44 +2,46 @@
 #include <string.h>
 #define MAX 1024
 
-void usage() {
+void usage()
+{
     printf("Usage: ./a.out <filename> <word>\n");
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     FILE *fp;
     char fline[MAX];
     char *newline;
     int count = 0;
     int occurrences = 0;
 
-    if (argc != 3) {
+    if (argc != 3)
+    {
         usage();
-        return 1;
+        exit(1);
     }
 
-    fp = fopen(argv[1], "r");  // Open the file in read mode.
-    if (fp == NULL) {
+    fp = fopen(argv[1], "r");
+    if (fp == NULL)
+    {
         printf("grep: Could not open file: %s\n", argv[1]);
-        return 1;
+        exit(1);
     }
-
-    // Read the file line by line.
-    while (fgets(fline, MAX, fp) != NULL) {
-        count++;  // Line number.
-
-        // Remove the newline character from the line.
-        if ((newline = strchr(fline, '\n')) != NULL) {
+    
+    while (fgets(fline, MAX, fp) != NULL)
+    {
+        count++;
+        if ((newline = strchr(fline, '\n')) != NULL)
+        {
             *newline = '\0';
         }
-
-        // Check if the specified word exists in the line.
-        if (strstr(fline, argv[2]) != NULL) {
+        if (strstr(fline, argv[2]) != NULL)
+        {
             printf("%s: %d %s\n", argv[1], count, fline);
             occurrences++;
         }
     }
 
-    fclose(fp);  // Close the file.
+    fclose(fp);
     return 0;
 }
