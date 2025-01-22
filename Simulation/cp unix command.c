ba604d128c0cb1ccf6af0409e0c0af5fe66f75c0
Mahesh Raghava <160461240+MaheshRaghava@users.noreply.github.com>
2025-01-22 12:04:31 +0530

    Update cp unix command.c

--- Simulation/cp unix command.c
+++ Simulation/cp unix command.c
@@ -2,17 +2,20 @@
 #include <unistd.h>
 #include <stdio.h>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     FILE *fp;
     char ch;
     int sc = 0;
-
-    fp = fopen(argv[1], "r");  // Open the source file in read mode.
-    if (fp == NULL) {
+    fp = fopen(argv[1], "r"); 
+    if (fp == NULL)
+    {
         printf("Unable to open the file: %s\n", argv[1]);
-    } else {
-        // Read the file character by character and count spaces.
-        while (!feof(fp)) {
+    }
+    else
+    {
+        while (!feof(fp))
+        {
             ch = fgetc(fp);
             if (ch == ' ')
                 sc++;
