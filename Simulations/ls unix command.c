684a85130b7cf2b288c4768cdcbf42cfce8721aa
Mahesh Raghava <160461240+MaheshRaghava@users.noreply.github.com>
2025-01-22 12:12:08 +0530

    Update ls unix command.c

--- Simulations/ls unix command.c
+++ Simulations/ls unix command.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
 #include <dirent.h>
 
-void main(int argc, char **argv) {
+void main(int argc, char **argv)
+{
     DIR *dp;
     struct dirent *link;
 
-    dp = opendir(argv[1]);  // Open the specified directory.
-    if (dp == NULL) {
+    dp = opendir(argv[1]);
+    if (dp == NULL)
+    {
         printf("Unable to open directory: %s\n", argv[1]);
         return;
     }
 
     printf("Contents of the directory %s are:\n", argv[1]);
-    while ((link = readdir(dp)) != NULL) {
-        printf("%s\n", link->d_name);  // Print each file or folder name.
+    while ((link = readdir(dp)) != NULL)
+    {
+        printf("%s\n", link->d_name);
     }
-    closedir(dp);  // Close the directory.
+    closedir(dp);
 }
