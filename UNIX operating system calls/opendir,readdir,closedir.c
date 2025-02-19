1d50dbb439136dbdb46f5628de3ebdce679cb2a6
Mahesh Raghava <160461240+MaheshRaghava@users.noreply.github.com>
2025-02-19 11:26:56 +0530

    Update opendir,readdir,closedir.c

--- UNIX operating system calls/opendir,readdir,closedir.c
+++ UNIX operating system calls/opendir,readdir,closedir.c
@@ -1,18 +1,25 @@
-#include<stdio.h>
-#include<dirent.h>
-#include<stdlib.h>
-struct dirent *dptr;
-int main(int argc,char *argv[]){
-  char buff[100];
-  DIR *dirp;
-  printf("\n\n ENTER DIRECTORY NAME:");
-  scanf("%s",buff);
-  if((dirp=opendir(buff))==NULL){
-     printf("The given directory does not exist");
-     exit(1);
-  }
-  while(dptr=readdir(dirp)){
-    printf("%s\n",dptr->d_name);
-  }
-  closedir(dirp);
+#include <stdio.h>
+#include <stdlib.h>
+#include <dirent.h>
+int main() {
+    char buff[100];
+    DIR *dirp;
+    struct dirent *dptr;  // Declare inside main, not globally
+    printf("\n\nEnter directory name: ");
+    scanf("%s", buff);
+    // Try opening the directory
+    dirp = opendir(buff);
+    if (dirp == NULL) {
+        printf("The given directory does not exist.\n");
+        exit(1);
+    }
+
+    // Read and print directory contents
+    printf("Contents of directory %s:\n", buff);
+    while ((dptr = readdir(dirp)) != NULL) {
+        printf("%s\n", dptr->d_name);
+    }
+    // Close the directory stream
+    closedir(dirp);
+    return 0;
 }
