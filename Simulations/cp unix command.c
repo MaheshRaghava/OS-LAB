2b14578ca8986c512b998d6334369a5757bfc54f
Mahesh Raghava <160461240+MaheshRaghava@users.noreply.github.com>
2025-02-19 11:34:09 +0530

    Update cp unix command.c

--- Simulations/cp unix command.c
+++ Simulations/cp unix command.c
@@ -6,17 +6,21 @@ int main(int argc, char *argv[])
     char ch;
     int sc = 0;
 
-    if (argc != 2) {
+    if (argc != 2) 
+    {
         printf("Usage: %s <filename>\n", argv[0]);
         return 1;
     }
 
     fp = fopen(argv[1], "r");
-    if (fp == NULL) {
+    if (fp == NULL) 
+    {
         printf("Unable to open the file: %s\n", argv[1]);
-    } else {
+    } else 
+    {
         while ((ch = fgetc(fp)) != EOF) {
-            if (ch == ' ') {
+            if (ch == ' ') 
+            {
                 sc++;
             }
         }
